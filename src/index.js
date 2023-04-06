47c1f7c7dd8b95416e1d95c81438ba8b28738bd7
Maedea9 <melaniecascante.j@gmail.com>
2023-04-06 11:38:42 -0500

    Add function for adding task with click and enter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
-import './style.css';
+/*import './style.css';*/
 
-const tasksToDo = [
+/*const tasksToDo = [
   {
-    description: 'task to do 1',
+    description: '',
     completed: false,
     index: 1,
   },
   {
-    description: 'task to do 2',
+    description: '',
     completed: false,
     index: 2,
   },
   {
-    description: 'task to do 3',
+    description: '',
     completed: false,
     index: 3,
   },
-];
+];*/
 
-const taskList = document.getElementById('task-list');
-const displayTasks = () => {
+//add new tasks//
+
+/*const add = () => {
+  if (addNewTask.value !== '') {
+      const tasksToDo = JSON.parse(localStorage.getItem('tasksToDo')) || [];
+      tasksToDo.push({completed: false, description: addNewTask.value});
+      for (let i = 1; i <= tasksToDo.length; i += 1) { 
+          tasksToDo[i - 1].index = i;
+      }
+      localStorage.setItem('tasksToDo', JSON.stringify(tasksToDo));
+  }
+  addNewTask.value = '';
+};
+
+const addIcon = document.querySelector('.submit'); 
+const addNewTask = document.querySelector('.new-task');
+
+addIcon.addEventListener('click', () => {
+  add();
+});
+
+addNewTask.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) { 
+      add();
+  }
+});*/
+
+/*const taskList = document.getElementById('task-list');*/
+/*const displayTasks = () => {
   tasksToDo.forEach((task) => {
     const taskBox = document.createElement('div');
     taskBox.classList = 'task-box-css';
@@ -32,4 +59,27 @@ const displayTasks = () => {
   });
 };
 
-displayTasks();
+displayTasks();*/
+
+///New task //
+const addTask = (taskInput, taskList) => {
+  const task = taskInput.value;
+  const newTask = document.createElement('div');
+  newTask.textContent = task;
+  taskList.appendChild(newTask);
+  taskInput.value = "";
+};
+
+const addIcon = document.getElementById("add-icon");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addIcon.addEventListener("click", () => addTask(taskInput, taskList));
+
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();// prevents page reload
+    addTask(taskInput, taskList);
+  }
+});
+addTask();
