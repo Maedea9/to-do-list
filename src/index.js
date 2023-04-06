4f84b38fd91b7aa195fade71af3d8c83825a8e6d
Maedea9 <melaniecascante.j@gmail.com>
2023-04-06 13:40:10 -0500

    Implement Edit. Add completed and index properties

--- src/index.js
+++ src/index.js
@@ -2,55 +2,18 @@
 
 const tasksToDo = [];
 
-
 //add new tasks//
 
-/*const add = () => {
-  if (addNewTask.value !== '') {
-      const tasksToDo = JSON.parse(localStorage.getItem('tasksToDo')) || [];
-      tasksToDo.push({completed: false, description: addNewTask.value});
-      for (let i = 1; i <= tasksToDo.length; i += 1) { 
-          tasksToDo[i - 1].index = i;
-      }
-      localStorage.setItem('tasksToDo', JSON.stringify(tasksToDo));
-  }
-  addNewTask.value = '';
-};
-
-const addIcon = document.querySelector('.submit'); 
-const addNewTask = document.querySelector('.new-task');
-
-addIcon.addEventListener('click', () => {
-  add();
-});
-
-addNewTask.addEventListener('keydown', (event) => {
-  if (event.keyCode === 13) { 
-      add();
-  }
-});*/
-
-/*const taskList = document.getElementById('task-list');*/
-/*const displayTasks = () => {
-  tasksToDo.forEach((task) => {
-    const taskBox = document.createElement('div');
-    taskBox.classList = 'task-box-css';
-    taskBox.innerHTML = ` <div class="task-activity">
-                    <input type="checkbox" class="input-check">
-                    <input type="text" class="text-input" value="${task.description}">
-                  </div>
-                  <i class="move-icon" id="order-icon">&#x22EE;</i>`;
-    taskList.appendChild(taskBox);
-  });
-};
-
-displayTasks();*/
-
-///New task //
 const addTask = (taskInput, taskList) => {
   const task = taskInput.value;
   const newTask = document.createElement('div');
   newTask.textContent = task;
+
+  const taskObject = {
+    description: task,
+    completed: false,
+    index: taskList.childElementCount + 1
+  };
 
   // Create taskBox element
   const taskBox = document.createElement('div');
@@ -64,11 +27,27 @@ const addTask = (taskInput, taskList) => {
   // Append taskBox to taskList
   taskList.appendChild(taskBox);
 
+  //add event listener for editing a task//
+  const textInput = taskBox.querySelector('.text-input');
+  textInput.addEventListener('change', (e) => {
+    const editedTask = e.target.value;
+    updateTask(task, editedTask);
+  });
+
   taskInput.value = "";
 
   const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  tasks.push(task);
+  tasks.push(taskObject); //push taskObject to task array
   localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const updateTask = (oldTask, newTask) => {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  const index = tasks.indexOf(oldTask);
+  if (index !== -1) {
+    tasks[index] = newTask;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 };
 
 const addIcon = document.getElementById("add-icon");
@@ -98,5 +77,12 @@ window.addEventListener("load", () => {
 
     // Append taskBox to taskList
     taskList.appendChild(taskBox);
+
+    // Add event listener for editing tasks
+    const textInput = taskBox.querySelector('.text-input');
+    textInput.addEventListener('change', (e) => {
+      const editedTask = e.target.value;
+      updateTask(task, editedTask);
+    });
   });
 });
