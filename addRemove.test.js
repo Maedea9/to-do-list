3d335c9967251721e3e9602fc125ff466220b333
Maedea9 <melaniecascante.j@gmail.com>
2023-04-20 10:43:30 -0500

    Test deleteTask function

--- addRemove.test.js
+++ addRemove.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { addTask } from './src/modules/functionality.js';
+import { addTask, deleteTask } from './src/modules/functionality.js';
 
 //isolated environment with no tasks
 document.body.innerHTML = '<div class="main"><h1>To Do List</h1><hr><!--To do list--><div class="task-container"><div class="task-header"><h2>Todays To Do</h2><i class="clockwise">&#8635;</i></div><div><form class="add-task-form"><input id="taskInput" class="new-task" type="text" placeholder="Add to your list..."><i id="add-icon" class="submit">&#8617;</i></div><div id="taskList" class="list-content"><!--task list created dynamically--></div></form><div class="clear-tasks"><button id="clear-complete" class="clear-all-complete" type="button">Clear all completed</button></div></div></div>'
@@ -17,4 +17,11 @@ describe('addTask function', () => {
         expect(container.children.length).toEqual(1);
       }, 1000);
     });
+});
+
+describe('deleteTask function', () => {
+  test('should remove exactly one task from the container', () => {
+    deleteTask();
+    expect(container.children.length).toBe(0)
+  });
 });
