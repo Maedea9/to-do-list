6ab08825b53995176b21e538e0c2fdea19d68a0e
Maedea9 <melaniecascante.j@gmail.com>
2023-04-20 19:54:44 -0500

    Complete checkbox test

--- updateComplete.test.js
+++ updateComplete.test.js
@@ -29,68 +29,44 @@ describe('clearAllCompleted', () => {
   });
 });
 
-jest.mock('./localStorage', () => ({
+//CHECKBOX TEST//
+
+jest.mock('./src/modules/storage.js', () => ({
   getItem: jest.fn(() => '[{"description":"Task 1","completed":false},{"description":"Task 2","completed":false}]'),
   setItem: jest.fn(),
 }));
 
 describe('renderTasks', () => {
-  let taskList;
-
   beforeEach(() => {
-    // Create a mock taskList element
-    taskList = document.createElement('div');
-    taskList.id = 'taskList';
-    document.body.appendChild(taskList);
+    // Create some test data to use for localStorage
+    const testToDoArray = [
+      { description: 'Task 1', completed: false },
+      { description: 'Task 2', completed: true },
+    ];
+    localStorage.setItem('toDoArray', JSON.stringify(testToDoArray));
   });
 
-  afterEach(() => {
-    // Clean up taskList element after each test
-    taskList.remove();
-    localStorage.clear();
-  });
+  describe('COMPLETED TASKS', () => {
+    test('should mark a task as completed in localStorage when checkbox is clicked', () => {
+      // Render the tasks to the DOM
+      document.body.innerHTML = `
+        <div id="taskList"></div>
+      `;
+      renderTasks();
 
-  test('renders tasks from localStorage', () => {
-    renderTasks();
+      // Wait for the tasks to be rendered
+      setTimeout(() => {
+        // Simulate a click on the checkbox for Task 1
+        const checkbox = document.querySelector('.input-check');
+        checkbox.checked = true;
+        checkbox.dispatchEvent(new Event('click'));
 
-    // Verify that taskList contains two task-box-css elements
-    expect(taskList.querySelectorAll('.task-box-css').length).toBe(2);
+        // Check that the completed state of Task 1 was updated in localStorage
+        const toDoArray = JSON.parse(localStorage.getItem('toDoArray'));
+        expect(toDoArray[0].completed).toBe(true);
 
-    // Verify that taskList contains two input-check elements
-    expect(taskList.querySelectorAll('.input-check').length).toBe(2);
-
-    // Verify that the first input-check element is not checked
-    expect(taskList.querySelectorAll('.input-check')[0].checked).toBe(false);
-
-    // Verify that the second input-check element is not checked
-    expect(taskList.querySelectorAll('.input-check')[1].checked).toBe(false);
-
-    // Verify that localStorage.setItem() was called once with the correct arguments
-    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
-  });
-
-  test('renders no tasks when localStorage is empty', () => {
-    localStorage.setItem('toDoArray', '[]');
-
-    renderTasks();
-
-    // Verify that taskList contains no task-box-css elements
-    expect(taskList.querySelectorAll('.task-box-css').length).toBe(0);
-
-    // Verify that localStorage.setItem() was not called
-    expect(localStorage.setItem).not.toHaveBeenCalled();
-  });
-
-  test('sets task as completed in localStorage when checkbox is clicked', () => {
-    renderTasks();
-
-    // Click the first input-check element
-    taskList.querySelectorAll('.input-check')[0].click();
-
-    // Verify that localStorage.setItem() was called once with the correct arguments
-    expect(localStorage.setItem).toHaveBeenCalledWith(
-      'toDoArray',
-      '[{"description":"Task 1","completed":true},{"description":"Task 2","completed":false}]',
-    );
+        done();
+      }, 1000);
+    });
   });
 });
