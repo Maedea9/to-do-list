453cbc19f7d3a93d9d10a5e3a671e39aa8bd3952
Maedea9 <melaniecascante.j@gmail.com>
2023-04-20 20:52:12 -0500

    Complete test for checkbox

--- updateComplete.test.js
+++ updateComplete.test.js
@@ -24,3 +24,45 @@ describe('clearAllCompleted', () => {
     ]);
   });
 });
+
+// CHECKBOX TEST//
+
+jest.mock('./src/modules/storage.js', () => ({
+  getItem: jest.fn(() => '[{"description":"Task 1","completed":false},{"description":"Task 2","completed":false}]'),
+  setItem: jest.fn(),
+}));
+
+describe('renderTasks', () => {
+  beforeEach(() => {
+    // Create some test data to use for localStorage
+    const testToDoArray = [
+      { description: 'Task 1', completed: false },
+      { description: 'Task 2', completed: true },
+    ];
+    localStorage.setItem('toDoArray', JSON.stringify(testToDoArray));
+  });
+
+  describe('COMPLETED TASKS', () => {
+    test('should mark a task as completed in localStorage when checkbox is clicked', () => {
+      // render the tasks to DOM
+      document.body.innerHTML = `
+        <div id="taskList"></div>
+      `;
+      renderTasks();
+
+      // use setTimeout to wait for tasks to be rendered
+      setTimeout(() => {
+        // simulation of click on the checkbox for Task 1
+        const checkbox = document.querySelector('.input-check');
+        checkbox.checked = true;
+        checkbox.dispatchEvent(new Event('click'));
+
+        // check that completed state of Task 1 has been updated in localStorage
+        const toDoArray = JSON.parse(localStorage.getItem('toDoArray'));
+        expect(toDoArray[0].completed).toBe(true);
+
+        done();
+      }, 1000);
+    });
+  });
+});
